9ea5b779cd3f6137bea645c731a983cbd0054230
Patrick2618 <camachoo2618@gmail.com>
2023-10-10 21:55:57 -0800

    Correcciion de caracteres raros en el codigo de Login.tsx y Login.css que no dejaban correr el programa

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,19 +6,15 @@ import { Principal } from "../Principal/Principal";
 
 import "./Login.css";
 
+
+
 export const Login = () => {
   const { handleChangePage } = useContext(PrincipalContext);
 
 
 
-
-
-
-  
-
   return (
     <>
-<<<<<<< HEAD
       <h2 className="login-title titles">Full Kong</h2>
       <div className="login-container">
         <div>
@@ -29,25 +25,10 @@ export const Login = () => {
             <pre className="login-registrarse">
               {" "}
               ¿Aún no tienes una cuenta?
-=======
-    
-      <h2 className="login-title titles">Full Kong</h2>
-      <div className="login-container">
-
-        <div>
-
-          <form className="login-form">
-
-            <input type="text" name="usuario" placeholder="Usuario"/>
-            <input type="password" name="password" placeholder="Contraseña"/>
-
-            <pre className="login-registrarse"> ¿Aún  no tienes una cuenta? 
->>>>>>> e60bd0f522f9f54d83cbbc15742eac94ff20b609
               <button
                 type="button"
                 onClick={() => {
                   handleChangePage(<Register />);
-<<<<<<< HEAD
                 }}
               >
                 Registrate
@@ -63,25 +44,9 @@ export const Login = () => {
             >
               Entrar
             </button>
-=======
-                }}>
-                 Registrate
-              </button>
-            </pre>
-
-            <button className="login-iniciar text"
-                type="button"
-                onClick={() => {
-                  handleChangePage(<Register />);
-                }}>
-                 Entrar
-              </button>
-                
->>>>>>> e60bd0f522f9f54d83cbbc15742eac94ff20b609
           </form>
         </div>
-
       </div>
     </>
   );
-};
+}
